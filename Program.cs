a1d5e1e016165728d97d4febb9548223236d0ea4
SUMMIT KHATIWADA <summitkhatiwada@Khatiwadas-MacBook-Pro.local>
2022-08-14 11:27:00 -0500

    Added edit and delete features

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace NotesApplication{
             // Creating a list of notes to add individual notes
             List<Note> allNotes = new List<Note>();
 
-            // Date for note
+            // Date for note - TODO : Fix date issue
             DateTime noteDate = DateTime.Now;
             Console.WriteLine(noteDate);
 
@@ -50,6 +50,51 @@ namespace NotesApplication{
                         Console.WriteLine(allNotes[i].content);
                     }
                 }
+
+                // EDIT NOTES
+                if (selection == 3){
+                    // Displaying the date, note id and topic only for selestion
+                    for (int i=0; i < allNotes.Count; i++){
+                        Console.WriteLine("");
+                        Console.WriteLine(allNotes[i].date + " " + allNotes[i].id + " " + allNotes[i].topic);
+                    }
+                    Console.WriteLine("");
+                    Console.Write("Note ID you would like to EDIT :: ");
+                    
+                    // Taking user input for the note they want to edit
+                    int editSelection = Convert.ToInt32(Console.ReadLine());
+
+                    // Editing the topic of selected note
+                    Console.WriteLine("");
+                    Console.Write("Topic :: ");
+                    string noteTopic = Console.ReadLine();
+                    Console.Write("Content :: ");
+                    string noteContent = Console.ReadLine();
+
+                    // Inserting the edited note
+                    allNotes[editSelection - 1].topic = noteTopic;
+                    allNotes[editSelection - 1].content = noteContent;
+                }
+
+                // DELETE NOTE
+                if (selection == 4){
+                    // Displaying the date, note id and topic only for selestion
+                    for (int i=0; i < allNotes.Count; i++){
+                        Console.WriteLine("");
+                        Console.WriteLine(allNotes[i].date + " " + allNotes[i].id + " " + allNotes[i].topic);
+                    }
+                    Console.WriteLine("");
+                    Console.Write("Note ID you would like to DELETE :: ");
+                    
+                    // Taking user input for the note they want to edit
+                    int deleteSelection = Convert.ToInt32(Console.ReadLine());
+
+                    // Removing the note
+                    allNotes.Remove(allNotes[deleteSelection - 1]);
+
+                }
+
+
 
             }            
 
